5ada97112d03e3da18275d2fe345e72705dcdafa
VincentYJk <601810068@qq.com>
2017-10-23 15:03:10 +0800

    jason yan --- comple most of key parts of custom DispatchServlet of SpringMVC ,and add some log info.

--- src/main/java/com/pwc/springMVC/servlet/CustomDispatchSeverlet.java
+++ src/main/java/com/pwc/springMVC/servlet/CustomDispatchSeverlet.java
@@ -1,6 +1,7 @@
 package com.pwc.springMVC.servlet;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -10,7 +11,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.pwc.springMVC.annotation.Controller;
 import com.pwc.springMVC.annotation.RequestMapping;
 import com.pwc.springMVC.util.ClassScanner;
@@ -18,32 +20,40 @@ import com.pwc.springMVC.util.ClassScanner;
 /**
  * Servlet implementation class DispatchSeverlet
  */
-/*@WebServlet(
-		urlPatterns = { "/severletTesting" }, 
-		initParams = { 
-				@WebInitParam(name = "basePackage", value = "com.pwc.springMVC", description = "scanner URL")
-		})*/
+/*
+ * @WebServlet( urlPatterns = { "/severletTesting" }, initParams = {
+ * 
+ * @WebInitParam(name = "basePackage", value = "com.pwc.springMVC", description
+ * = "scanner URL") })
+ */
 public class CustomDispatchSeverlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static Logger log=LoggerFactory.getLogger(CustomDispatchSeverlet.class);
+	
 	private Map<String, Object> controllers = new HashMap<String, Object>();
 	private Map<String, Method> methods = new HashMap<String, Method>();
-    /**
-     * Default constructor. 
-     */
-    public CustomDispatchSeverlet() {
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * Default constructor.
+	 */
+	public CustomDispatchSeverlet() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
 	public void init(ServletConfig config) throws ServletException {
+		log.debug("init Servlet to pulling target(Controllers and Mtethods)");
+		
 		Map<String, Class<?>> results = new HashMap<String, Class<?>>();
 		results = ClassScanner.scannerClass(config.getInitParameter("basePackage"));
-
+		log.debug("Loop the results get from util ClassScanner to create mapping relationship for Controllers(class name -> class's instance), Methods(requestMapping path -> method)");
 		Iterator<String> keys = results.keySet().iterator();
 
 		while (keys.hasNext()) {
+
+			String key = keys.next();
 			Class clazz = results.get(keys.next());
 
 			if (clazz.isAnnotationPresent(Controller.class)) {
@@ -54,9 +64,9 @@ public class CustomDispatchSeverlet extends HttpServlet {
 					RequestMapping requestMapping = (RequestMapping) clazz.getAnnotation(RequestMapping.class);
 					path = requestMapping.value();
 				}
-				
+
 				try {
-					controllers.put(keys.next(), clazz.newInstance());
+					controllers.put(clazz.getName(), clazz.newInstance());
 				} catch (InstantiationException e) {
 					e.printStackTrace();
 				} catch (IllegalAccessException e) {
@@ -72,21 +82,38 @@ public class CustomDispatchSeverlet extends HttpServlet {
 
 					System.out.println("print each of url of method: " + path
 							+ method.getAnnotation(RequestMapping.class).value());
-					
-					methods.put(path+method.getAnnotation(RequestMapping.class).value(), method);
+
+					methods.put(path + method.getAnnotation(RequestMapping.class).value(), method);
 				}
 			}
-			
 
 		}
 		
+		log.debug("End Servlet to pulling target(Controllers and Mtethods)");
+
 	}
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		System.out.println(req.getRequestURI());
-		super.service(req, resp);
+		// /springMVC/abc.do
+		String url = req.getRequestURI();
+		String contexPath = req.getContextPath();
+		String mappingPath = url.substring(url.indexOf(contexPath) + contexPath.length(), url.indexOf(".do"));
+
+		Method method = methods.get(mappingPath);
+		Object controller = controllers.get(method.getDeclaringClass().getName());
+		try {
+			method.invoke(controller);
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/main/java/com/pwc/springMVC/util/ClassScanner.java
+++ src/main/java/com/pwc/springMVC/util/ClassScanner.java
@@ -7,95 +7,108 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class ClassScanner {
 
-public static Map<String, Class<?>> scannerClass(String basePackage){
+	private static Logger log = LoggerFactory.getLogger(ClassScanner.class);
+
 	
-	 Map<String, Class<?>> results = new HashMap<String, Class<?>>();
-	 //com.pwc.springmvc
-	String filePath= basePackage.replace(".", "/");
-	// com/pwc/springmvc
-	System.out.println(filePath);
-	try {
-		Enumeration<URL> dirs = Thread.currentThread().getContextClassLoader().getResources(filePath);
+	
+	/**
+	 * parameter: basePackage Path
+	 * return result:all of class (Map<String, Class<?>>)
+	 *
+	 * */
+	public static Map<String, Class<?>> scannerClass(String basePackage) {
+
+		log.debug("Start to pulling all of class under the base package.");
 		
-		//  /C:/yanjikun_work/Eclipse_WorkSpace/JavaBasic/bin/com/pwc/springmvc
-		String rootPath = Thread.currentThread().getContextClassLoader().getResource(filePath).getPath();
-		
-		System.out.println(rootPath);
-		
-		if(rootPath!=null) {
-			rootPath = rootPath.substring(rootPath.lastIndexOf(filePath));
-		}
-		System.out.println(rootPath);
-		while (dirs.hasMoreElements()) {
-			URL url = (URL) dirs.nextElement();
-			//  file:/C:/yanjikun_work/Eclipse_WorkSpace/JavaBasic/bin/com/pwc/springmvc
-			System.out.println(url);
-			if(url.getProtocol().equals("file")) {
-				File file  =new File(url.getPath().substring(1));
-				
-				scannerFiles(file,rootPath,results);
+		Map<String, Class<?>> results = new HashMap<String, Class<?>>();
+		// com.pwc.springmvc
+		String filePath = basePackage.replace(".", "/");
+		// com/pwc/springmvc
+		System.out.println(filePath);
+		try {
+			Enumeration<URL> dirs = Thread.currentThread().getContextClassLoader().getResources(filePath);
+
+			// /C:/yanjikun_work/Eclipse_WorkSpace/JavaBasic/bin/com/pwc/springmvc
+			String rootPath = Thread.currentThread().getContextClassLoader().getResource(filePath).getPath();
+
+			System.out.println(rootPath);
+
+			if (rootPath != null) {
+				rootPath = rootPath.substring(rootPath.lastIndexOf(filePath));
+			}
+			System.out.println(rootPath);
+			while (dirs.hasMoreElements()) {
+				URL url = (URL) dirs.nextElement();
+				// file:/C:/yanjikun_work/Eclipse_WorkSpace/JavaBasic/bin/com/pwc/springmvc
+				System.out.println(url);
+				if (url.getProtocol().equals("file")) {
+					File file = new File(url.getPath().substring(1));
+
+					scannerFiles(file, rootPath, results);
+				}
+
 			}
 			
-		}
-		
-		
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}	
-	
-	
-	
-	
-	return results;
-	
-}
+			log.debug("End to pulling all of class under the base package.");
 
-private static void scannerFiles(File floder,String rootPath,Map<String,Class<?>> classes) {
-	
-	File[] files = floder.listFiles();
-	String rootP = rootPath;
-	
-	for (int i = 0; files!=null&&i < files.length ; i++) {
-		File file = files[i];
-		if(file.isDirectory()) {
-			
-			scannerFiles(file,rootP+file.getName()+"/",classes);
-		}else {
-			String path = file.getAbsolutePath();
-			if(path.endsWith(".class")) {
-				
-				path = path.replace("\\", "/");
-				
-				String className = rootP.substring(0, rootP.lastIndexOf("/"))+ path.substring(path.lastIndexOf("/"), path.indexOf(".class"));
-				
-				className = className.replace("/", ".");
-				
-				System.out.println(className);
-		try {
-			classes.put(className, Class.forName(className));
-		} catch (ClassNotFoundException e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
-				
-			}
-			
 		}
-		
-		
+
+		return results;
+
 	}
+
+	private static void scannerFiles(File floder, String rootPath, Map<String, Class<?>> classes) {
+
+		File[] files = floder.listFiles();
+		String rootP = rootPath;
+
+		for (int i = 0; files != null && i < files.length; i++) {
+			File file = files[i];
+			if (file.isDirectory()) {
+
+				scannerFiles(file, rootP + file.getName() + "/", classes);
+			} else {
+				String path = file.getAbsolutePath();
+				if (path.endsWith(".class")) {
+
+					path = path.replace("\\", "/");
+
+					String className = rootP.substring(0, rootP.lastIndexOf("/"))
+							+ path.substring(path.lastIndexOf("/"), path.indexOf(".class"));
+
+					className = className.replace("/", ".");
+
+					System.out.println(className);
+					try {
+						classes.put(className, Class.forName(className));
+					} catch (ClassNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+
 	
-	
-}
-public static void  main(String[] args) {
-	
-	
-	ClassScanner.scannerClass("com.pwc.springMVC");
-	
-}
+	// this method is used for local testing. 
+	public static void main(String[] args) {
+
+		ClassScanner.scannerClass("com.pwc.springMVC");
+
+	}
 
 }
